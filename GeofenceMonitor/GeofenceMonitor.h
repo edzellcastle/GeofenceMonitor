9435e4911c4013ef3566d559b8671f3b680259db
David Lindsay <davidlindsay100@icloud.com>
2021-04-16 08:42:10 -0700

    Initial Commit

---
+++ GeofenceMonitor/GeofenceMonitor.h
@@ -0,0 +1,18 @@
+//
+//  GeofenceMonitor.h
+//  GeofenceMonitor
+//
+//  Created by David Lindsay on 4/16/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GeofenceMonitor.
+FOUNDATION_EXPORT double GeofenceMonitorVersionNumber;
+
+//! Project version string for GeofenceMonitor.
+FOUNDATION_EXPORT const unsigned char GeofenceMonitorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeofenceMonitor/PublicHeader.h>
+
+
